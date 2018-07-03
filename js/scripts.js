ad09c64d26efd8c4ed14fb54e7ac0de514e1a8a0
unknown <aguilarjacob23@gmail.com>
2018-07-03 14:35:14 -0700

    Day 1 Take Home complete.

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,42 @@ firstName = "Jacob"; //string
 //Define and declare Variable
 var lastName = "Aguilar";
 
+//console readout of variables firstName and lastName
 console.log(firstName, lastName);
 
-firstName = "Batman";
+//What i want to get out of JS1 and Bitwise in general. Its a bit lengthy so i am sorry!!
 
+//After looking at the console the text has the line breaks in weird places as i thought it would be. I am not sure how to write multiline strings
+//so that they appear neat and organized, definitely a question for next class.
+var classDesires = "CLASS DESIRES: After receiving my degree in IT/Software Engineering from\
+                    University of Phoenix this past April, i honestly felt as if i accomplished\
+                    nothing. I felt as if i did not learn how to develop software \
+                    let alone a website from start to finish. However, i am lucky \
+                    that i live in FresYes, and have the opportunity to attend \
+                    Bitwise/GeekWise. I want to make a career out of doing somthing \
+                    that i love to learn about. I love the idea of literally having \
+                    nothing but a concept and turning that into a piece of tech. \
+                    I also fell on hard times recently, after having multiple differnt jobs \
+                    i found a job that i thought i could make money selling insurance. \
+                    After 6 months, things started going south. As i had moved out of my parents place \
+                    before i saved up enough money to support myself. I fell behind on my rent, \
+                    car payment, and literally everything else you can think of. In the end, \
+                    i had to move back in with my parents, and that was demoralizing to say the least \
+                    Now i have realized that i WANT to become a developer so much, just so i can live \
+                    the life i want to live. Sorry, kind of went off on a tangent there but i felt \
+                    it was necessary to explain my past to vocalize just how badly i want to become a developer. \
+                    And that actual first step, was me stepping into room 3 of my first Bitwise class on July 2nd, 2018";
+
+//My favorite hobby to do when i am not working, learning, or spending time with family or friends.
+var favHobby = "FAVORITE HOBBY: While I do enjoy learning about software in general, i also LOVE to build PCs.\
+                In fact, i believe it is what really got me into technology in the first place.\
+                I remember having a friend help build my very first PC years ago, and now i love helping\
+                others build their PCs, whether it be a simple desktop for productivity, to balls to the wall\
+                gaming or workstation PCs.";
+
+console.log(classDesires);
+console.log(favHobby);
+
+/*firstName = "Batman";
 console.log(firstName, lastName);
+*/
