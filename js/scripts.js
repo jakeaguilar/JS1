f9a6ec8d760514adc4c79c6db81d21f298df395a
unknown <aguilarjacob23@gmail.com>
2018-07-02 21:01:32 -0700

    Day 2 work in progress.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+var myString = "Hello World*76";
+console.log(typeof myString);
+var myNumber = 43;
+console.log(typeof myNumber);
+
+//good way of naming bools is using is? like Is the user logged in?
+var isLoggedIn = false; //boolean
+
+//if you see a "variable" in the console on chrome is "not defined"
+var newVariable;
+console.log(newriable);// purposely wrong!! will give error above ^^
+
+//null varialble will literally just say null!
+//isLoggedIn = null;
