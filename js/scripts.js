ddad9b1863889e003093aea974b24da23621b6d0
unknown <aguilarjacob23@gmail.com>
2018-07-02 20:37:22 -0700

    Day 1 in class work completed.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+//Declare a Variable
+var firstName;
+
+//Define the Variable
+firstName = "Jacob"; //string
+
+//Define and declare Variable
+var lastName = "Aguilar";
+
+console.log(firstName, lastName);
+
+firstName = "Batman";
+
+console.log(firstName, lastName);
