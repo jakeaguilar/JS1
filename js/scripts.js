085094eef4351a8520d795e5bb82405c44bd7f63
unknown <aguilarjacob23@gmail.com>
2018-07-09 18:58:42 -0700

    Day 3 in progress

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,14 @@
-let bestNumber = 42;
+let bestNumber = true;
 let isTrue = false;
 
-console.log(bestNumber && isTrue);
+console.log(bestNumber || isTrue);
+
+
+let num = 11;
+let string = 11;
+
+//loose equality checks for value and NOT the type
+console.log(num == string);
+
+//strict equality checks for both value and type
+console.log(num === string);
