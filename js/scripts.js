df5959ef3131023ff2dc8a8148b02ff6bcf3cce5
unknown <aguilarjacob23@gmail.com>
2018-07-18 18:57:15 -0700

    Day 6 in progress

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,2 @@
-
+// The DOM is essentially a virtual space/environment that happens in your browser (individualized to every persons browser) where HTML/CSS, XML, JS, and JSON kind of meet.
+//Read the MDN on DOM
